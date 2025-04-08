b24e5e5d5bb5f6722b4198703978fe5d12b45589
Andres Court <andres@andrescourt.com>
2025-04-08 18:37:27 -0500

    feat(router): TanStack Router setup
    Documentation used:
    
    https://tanstack.com/router/latest/docs/framework/react/quick-start

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,22 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.tsx'
+
+import { routeTree } from './routeTree.gen'
+import { createRouter, RouterProvider } from '@tanstack/react-router'
+
+const router = createRouter({
+  routeTree
+})
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 )
+
+declare module '@tanstack/react-router' {
+  interface RegisterRouter {
+    router: typeof router
+  }
+}
