f208c71c5bc744f6daaead4e844e0fa6ea7931b4
Andres Court <andres@andrescourt.com>
2025-04-08 19:11:53 -0500

    feat(Tanstack Query): setup to use the library

--- src/main.tsx
+++ src/main.tsx
@@ -4,14 +4,20 @@ import './index.css'
 
 import { routeTree } from './routeTree.gen'
 import { createRouter, RouterProvider } from '@tanstack/react-router'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+
+const queryClient = new QueryClient()
 
 const router = createRouter({
-  routeTree
+  routeTree,
+  context: { queryClient },
 })
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <RouterProvider router={router} />
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router} />
+    </QueryClientProvider>
   </StrictMode>,
 )
 
--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,8 +1,12 @@
-import { Link, Outlet, createRootRoute } from '@tanstack/react-router'
+import { type QueryClient } from '@tanstack/react-query'
+import { Link, Outlet, createRootRouteWithContext } from '@tanstack/react-router'
 import { TanStackRouterDevtools } from '@tanstack/react-router-devtools'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 
 
-export const Route = createRootRoute({
+export const Route = createRootRouteWithContext<{
+  queryClient: QueryClient
+}>()({
   component: RootComponent,
 })
 
@@ -15,6 +19,7 @@ function RootComponent() {
       </nav>
       <Outlet />
       <TanStackRouterDevtools />
+      <ReactQueryDevtools />
     </>
   )
 }
