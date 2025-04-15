50a089cce6a9460b2e659064ebe27051d1c2114d
Andres Court <andres@andrescourt.com>
2025-04-15 17:50:54 -0500

    feat(header): navigate to the login page

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,10 +5,12 @@ export default function Header() {
 	return (
 		<header className="p-2 flex gap-2 bg-muted text-muted-foreground justify-between items-center">
 			<nav className="flex flex-row">
-				<div className="px-2 font-bold">
+				<div className="flex gap-3 px-2 font-bold">
 					<Link to="/" className="uppercase font-bold tracking-wide">
 						Sistema Control Presupuestario
 					</Link>
+
+					<Link to="/login">Login</Link>
 				</div>
 			</nav>
 
