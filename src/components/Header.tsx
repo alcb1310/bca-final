0f7f18b81262d67b48447012645d201db401e0be
Andres Court <andres@andrescourt.com>
2025-04-14 22:22:08 -0500

    feat(theme): use the theme toggler

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,13 +1,18 @@
 import { Link } from "@tanstack/react-router";
+import ThemeToggle from "./theme/theme-toggle";
 
 export default function Header() {
 	return (
-		<header className="p-2 flex gap-2 bg-white text-black justify-between">
+		<header className="p-2 flex gap-2 bg-muted text-muted-foreground justify-between items-center">
 			<nav className="flex flex-row">
 				<div className="px-2 font-bold">
-					<Link to="/">Home</Link>
+					<Link to="/" className="uppercase font-bold tracking-wide">
+						Sistema Control Presupuestario
+					</Link>
 				</div>
 			</nav>
+
+			<ThemeToggle />
 		</header>
 	);
 }
