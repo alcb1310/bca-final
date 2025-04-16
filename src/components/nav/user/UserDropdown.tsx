27fe29b70e4598b6f35aa18af7709ef21070fdd8
Andres Court <andres@andrescourt.com>
2025-04-15 21:40:18 -0500

    feat(nav): created user dropdown menu

---
+++ src/components/nav/user/UserDropdown.tsx
@@ -0,0 +1,36 @@
+import { Button } from '@/components/ui/button'
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from '@/components/ui/dropdown-menu'
+import { Link } from '@tanstack/react-router'
+import { User } from 'lucide-react'
+
+export default function UserDropdown() {
+  return (
+    <div>
+      <DropdownMenu>
+        <DropdownMenuTrigger>
+          <Button variant={'ghost'} size={'icon'}>
+            <User />
+          </Button>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent>
+          <DropdownMenuLabel>Usuarios</DropdownMenuLabel>
+          <DropdownMenuSeparator />
+          <DropdownMenuItem>
+            <Link to={'/usuarios/perfil'}>Perfil</Link>
+          </DropdownMenuItem>
+          <DropdownMenuItem>
+            <Link to={'/usuarios/admin'}>Administrar</Link>
+          </DropdownMenuItem>
+          <DropdownMenuItem>Cambiar contraseña</DropdownMenuItem>
+        </DropdownMenuContent>
+      </DropdownMenu>
+    </div>
+  )
+}
