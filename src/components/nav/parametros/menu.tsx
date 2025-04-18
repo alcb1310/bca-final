bdad29240e337a2923a0930bf0cc48c1b815303f
Andres Court <andres@andrescourt.com>
2025-04-18 08:51:42 -0500

    feat(nav): added links to the parameters menu

--- src/components/nav/parametros/menu.tsx
+++ src/components/nav/parametros/menu.tsx
@@ -4,6 +4,7 @@ import {
   CollapsibleTrigger,
 } from '@/components/ui/collapsible'
 import { SidebarMenuSub, SidebarMenuSubItem } from '@/components/ui/sidebar'
+import { Link } from '@tanstack/react-router'
 
 export default function ParametrosMenu() {
   return (
@@ -12,12 +13,24 @@ export default function ParametrosMenu() {
 
       <CollapsibleContent>
         <SidebarMenuSub>
-          <SidebarMenuSubItem>Partidas</SidebarMenuSubItem>
-          <SidebarMenuSubItem>Categorias</SidebarMenuSubItem>
-          <SidebarMenuSubItem>Materiales</SidebarMenuSubItem>
-          <SidebarMenuSubItem>Proyectos</SidebarMenuSubItem>
-          <SidebarMenuSubItem>Proveedores</SidebarMenuSubItem>
-          <SidebarMenuSubItem>Rubros</SidebarMenuSubItem>
+          <SidebarMenuSubItem>
+            <Link to='/parametros/categorias'>Categorias</Link>
+          </SidebarMenuSubItem>
+          <SidebarMenuSubItem>
+            <Link to='/parametros/materiales'>Materiales</Link>
+          </SidebarMenuSubItem>
+          <SidebarMenuSubItem>
+            <Link to='/parametros/partidas'>Partidas</Link>
+          </SidebarMenuSubItem>
+          <SidebarMenuSubItem>
+            <Link to='/parametros/proyectos'>Proyectos</Link>
+          </SidebarMenuSubItem>
+          <SidebarMenuSubItem>
+            <Link to='/parametros/proveedores'>Proveedores</Link>
+          </SidebarMenuSubItem>
+          <SidebarMenuSubItem>
+            <Link to='/parametros/rubros'>Rubros</Link>
+          </SidebarMenuSubItem>
         </SidebarMenuSub>
       </CollapsibleContent>
     </Collapsible>
