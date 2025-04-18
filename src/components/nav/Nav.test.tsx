dc135da4c9fb01365a9893c083d38a87a6444343
Andres Court <andres@andrescourt.com>
2025-04-18 09:37:37 -0500

    test(nav): renders all required transacciones options

--- src/components/nav/Nav.test.tsx
+++ src/components/nav/Nav.test.tsx
@@ -1,20 +1,43 @@
 import TestProvider from '@/__mocks__/provider'
 import { render, screen } from '@testing-library/react'
 import Nav from './Nav'
+Object.defineProperty(window, 'matchMedia', {
+  writable: true,
+  value: vitest.fn().mockImplementation((query) => ({
+    matches: false,
+    media: query,
+    onchange: null,
+    addListener: vitest.fn(), // deprecated
+    removeListener: vitest.fn(), // deprecated
+    addEventListener: vitest.fn(),
+    removeEventListener: vitest.fn(),
+    dispatchEvent: vitest.fn(),
+  })),
+})
 
 describe('Nav', () => {
   beforeEach(() => {
     render(<TestProvider component={Nav} />)
   })
 
-  it('should render', () => {
-    const home = screen.getByTestId('home-link')
-    expect(home).toBeInTheDocument()
-    expect(home).toHaveAttribute('href', '/')
-    expect(home).toHaveTextContent(/home/i)
+  it('should render the transacciones menu', () => {
+    const transacciones = screen.getByTestId('transacciones-menu')
+    expect(transacciones).toBeInTheDocument()
+    expect(transacciones).toHaveTextContent(/transacciones/i)
 
-    const login = screen.getByTestId('logout-link')
-    expect(login).toBeInTheDocument()
-    expect(login).toHaveTextContent(/salir/i)
+    const presupuesto = screen.getByTestId('transacciones-menu-presupuesto')
+    expect(presupuesto).toBeInTheDocument()
+    expect(presupuesto).toHaveTextContent(/presupuesto/i)
+    expect(presupuesto).toHaveAttribute('href', '/transacciones/presupuestos')
+
+    const factura = screen.getByTestId('transacciones-menu-facturas')
+    expect(factura).toBeInTheDocument()
+    expect(factura).toHaveTextContent(/facturas/i)
+    expect(factura).toHaveAttribute('href', '/transacciones/facturas')
+
+    const cierre = screen.getByTestId('transacciones-menu-cierre')
+    expect(cierre).toBeInTheDocument()
+    expect(cierre).toHaveTextContent(/cierre mensual/i)
+    expect(cierre).toHaveAttribute('href', '/transacciones/cierre-mensual')
   })
 })
