df55bca5f7bb0dd8bb228da32c89b4475b39c727
Andres Court <andres@andrescourt.com>
2025-04-18 12:13:59 -0500

    feat(nav): added a footer

--- src/components/nav/Nav.tsx
+++ src/components/nav/Nav.tsx
@@ -1,6 +1,7 @@
 import {
   Sidebar,
   SidebarContent,
+  SidebarFooter,
   SidebarHeader,
   SidebarMenu,
 } from '@/components/ui/sidebar'
@@ -19,6 +20,11 @@ export default function Nav() {
           <ParametrosMenu />
         </SidebarMenu>
       </SidebarContent>
+      <SidebarFooter>
+        <p className='text-xs text-primary/70 text-center'>
+          Copyright alcbsystems &copy; 2023 - {new Date().getFullYear()}
+        </p>
+      </SidebarFooter>
     </Sidebar>
   )
 }
