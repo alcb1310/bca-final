1c6702303aa6c28c88a8c9e4ac3bdb1a3d7fff7a
Andres Court <andres@andrescourt.com>
2025-04-18 08:26:45 -0500

    style(nav): added style to the navbar

--- src/components/ui/collapsible.tsx
+++ src/components/ui/collapsible.tsx
@@ -12,6 +12,7 @@ function CollapsibleTrigger({
   return (
     <CollapsiblePrimitive.CollapsibleTrigger
       data-slot="collapsible-trigger"
+      className="w-full rounded-md px-4 py-2 bg-primary text-primary-foreground shadow-xs hover:bg-primary/90 uppercase text-left"
       {...props}
     />
   )
--- src/components/ui/sidebar.tsx
+++ src/components/ui/sidebar.tsx
@@ -567,7 +567,7 @@ function SidebarMenuAction({
         "peer-data-[size=lg]/menu-button:top-2.5",
         "group-data-[collapsible=icon]:hidden",
         showOnHover &&
-          "peer-data-[active=true]/menu-button:text-sidebar-accent-foreground group-focus-within/menu-item:opacity-100 group-hover/menu-item:opacity-100 data-[state=open]:opacity-100 md:opacity-0",
+        "peer-data-[active=true]/menu-button:text-sidebar-accent-foreground group-focus-within/menu-item:opacity-100 group-hover/menu-item:opacity-100 data-[state=open]:opacity-100 md:opacity-0",
         className
       )}
       {...props}
@@ -658,7 +658,7 @@ function SidebarMenuSubItem({
     <li
       data-slot="sidebar-menu-sub-item"
       data-sidebar="menu-sub-item"
-      className={cn("group/menu-sub-item relative", className)}
+      className={cn("my-2 group/menu-sub-item relative uppercase tracking-wide text-xs", className)}
       {...props}
     />
   )
