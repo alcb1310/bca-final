217aa7efb902172b0030be49a7fe228739ed82d3
Andres Court <andres@andrescourt.com>
2025-04-18 22:28:18 -0500

    feat(landing): fetch logged in user information

--- src/routes/_auth/index.tsx
+++ src/routes/_auth/index.tsx
@@ -1,14 +1,42 @@
 import Greet from '@/components/greet/Greet'
-import { createFileRoute } from '@tanstack/react-router'
+import { getMe } from '@/queries/users'
+import { useSuspenseQuery } from '@tanstack/react-query'
+import { createFileRoute, redirect } from '@tanstack/react-router'
 
 export const Route = createFileRoute('/_auth/')({
   component: RouteComponent,
+  loader: async ({ context: { queryClient, auth } }) => {
+    const token = auth.token
+    if (!token) {
+      throw redirect({
+        to: '/login',
+      })
+    }
+
+    const user = await queryClient.prefetchQuery({
+      queryKey: ['users', 'me'],
+      queryFn: () => getMe({ token }),
+    })
+
+    return {
+      token,
+      user,
+    }
+  },
 })
 
 function RouteComponent() {
+  const { token } = Route.useLoaderData()
+  const meQuery = useSuspenseQuery({
+    queryKey: ['users', 'me'],
+    queryFn: () => getMe({ token }),
+  })
+
+  const user = meQuery.data
+
   return (
     <div>
-      <Greet name='Andres' />
+      <Greet name={user?.name} />
     </div>
   )
 }
