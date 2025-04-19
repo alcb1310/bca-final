51e26c33ab2f0f1342dedda3b2206a02a6bd100a
Andres Court <andres@andrescourt.com>
2025-04-18 22:29:06 -0500

    feat(greet): added propper styling

--- src/components/greet/Greet.tsx
+++ src/components/greet/Greet.tsx
@@ -1,7 +1,12 @@
 import { Button } from '../ui/button'
 
 export default function Greet({ name }: { name?: string }) {
-  if (name) return <h1>Bienvenido {name}</h1>
+  if (name)
+    return (
+      <h1>
+        Bienvenido <span className='text-success font-bold'>{name}</span>
+      </h1>
+    )
 
   return <Button>Login</Button>
 }
