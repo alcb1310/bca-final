765ad59ab11202e0de9f3342a2ceb7bd1fbf3313
Andres Court <andres@andrescourt.com>
2025-04-19 16:20:46 -0500

    feat(users): show all the users

--- src/routes/_auth/usuarios/admin.tsx
+++ src/routes/_auth/usuarios/admin.tsx
@@ -1,9 +1,81 @@
-import { createFileRoute } from '@tanstack/react-router'
+import { DataTable } from '@/components/ui/DataTable'
+import { getAllUsers } from '@/queries/users'
+import type { UserResponseType } from '@/types/users'
+import { useSuspenseQuery } from '@tanstack/react-query'
+import { createFileRoute, redirect } from '@tanstack/react-router'
+import type { ColumnDef } from '@tanstack/react-table'
+import { Pencil, Trash } from 'lucide-react'
 
 export const Route = createFileRoute('/_auth/usuarios/admin')({
   component: RouteComponent,
+  loader: async ({ context }) => {
+    const token = context.auth.token
+    if (!token) {
+      throw redirect({
+        to: '/login',
+      })
+    }
+
+    const users = await context.queryClient.prefetchQuery({
+      queryKey: ['users'],
+      queryFn: () => getAllUsers({ token }),
+    })
+
+    return {
+      users,
+      token,
+    }
+  },
 })
 
 function RouteComponent() {
-  return <div>Hello "/_auth/usuarios/admin"!</div>
+  const { token } = Route.useLoaderData()
+  const { data: users } = useSuspenseQuery({
+    queryKey: ['users'],
+    queryFn: () => getAllUsers({ token }),
+  })
+
+  const columns: ColumnDef<UserResponseType>[] = [
+    {
+      header: 'Nombre',
+      accessorKey: 'name',
+      size: 200,
+    },
+    {
+      header: 'Email',
+      accessorKey: 'email',
+    },
+    {
+      id: 'actions',
+      size: 50,
+      enableSorting: false,
+      enableHiding: false,
+      cell: ({ row: { original } }) => {
+        return (
+          <div className='flex items-center justify-center gap-2'>
+            <Pencil
+              size={12}
+              className='text-warning cursor-pointer'
+              onClick={() => {
+                console.log(original.id)
+              }}
+            />
+            <Trash
+              size={12}
+              className='text-destructive cursor-pointer'
+              onClick={() => {
+                console.log(original.id)
+              }}
+            />
+          </div>
+        )
+      },
+    },
+  ]
+
+  return (
+    <div>
+      <DataTable data={users} columns={columns} />
+    </div>
+  )
 }
