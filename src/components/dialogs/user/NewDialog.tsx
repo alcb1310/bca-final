cef6642ff92481d5c720b452f16a3faaa8070c1d
Andres Court <andres@andrescourt.com>
2025-04-20 23:34:28 -0500

    feat(user): create a new user

--- src/components/dialogs/user/NewDialog.tsx
+++ src/components/dialogs/user/NewDialog.tsx
@@ -3,16 +3,23 @@ import {
   Dialog,
   DialogClose,
   DialogContent,
+  DialogDescription,
   DialogFooter,
   DialogTitle,
   DialogTrigger,
 } from '@/components/ui/dialog'
 import { useAppForm } from '@/hooks/bca.form'
+import { addUser } from '@/queries/users'
 import { UserCreateSchema } from '@/types/users'
+import { type QueryClient, useMutation } from '@tanstack/react-query'
 import { Plus } from 'lucide-react'
 import { type FormEvent, useState } from 'react'
+import { toast } from 'sonner'
 
-export default function NewDialog() {
+export default function NewDialog({
+  token,
+  queryClient,
+}: Readonly<{ token: string; queryClient: QueryClient }>) {
   const [open, setOpen] = useState(false)
   const userForm = useAppForm({
     defaultValues: {
@@ -24,8 +31,21 @@ export default function NewDialog() {
       onSubmit: UserCreateSchema,
     },
     onSubmit: ({ value }) => {
-      console.log(value)
+      mutate({ token, user: value })
+    },
+  })
+
+  const { mutate, isError, error } = useMutation({
+    mutationFn: addUser,
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ['users'] })
+
+      // on success close the dialog and send toast message
+      toast('Usuario creado')
       setOpen(false)
+    },
+    onError: () => {
+      toast('Error al crear el usuario')
     },
   })
 
@@ -44,46 +64,52 @@ export default function NewDialog() {
       </DialogTrigger>
       <DialogContent>
         <DialogTitle>Crear usuario</DialogTitle>
-
-        <form
-          onSubmit={(e: FormEvent<HTMLFormElement>) => {
-            e.preventDefault()
-            e.stopPropagation()
-            console.log('submittig')
-            userForm.handleSubmit()
-          }}
-        >
-          <div aria-describedby='description' className='flex flex-col gap-4'>
-            <userForm.AppField name='email'>
-              {(field) => (
-                <field.TextField label='Email' placeholder='user@company.com' />
+        <DialogDescription asChild>
+          <form
+            onSubmit={(e: FormEvent<HTMLFormElement>) => {
+              e.preventDefault()
+              e.stopPropagation()
+              userForm.handleSubmit()
+            }}
+          >
+            <div aria-describedby='description' className='flex flex-col gap-4'>
+              {isError && userForm.state.isTouched && (
+                <p className='text-destructive'>{error?.message}</p>
               )}
-            </userForm.AppField>
+              <userForm.AppField name='email'>
+                {(field) => (
+                  <field.TextField
+                    label='Email'
+                    placeholder='user@company.com'
+                  />
+                )}
+              </userForm.AppField>
 
-            <userForm.AppField name='name'>
-              {(field) => (
-                <field.TextField label='Nombre' placeholder='Nombre' />
-              )}
-            </userForm.AppField>
+              <userForm.AppField name='name'>
+                {(field) => (
+                  <field.TextField label='Nombre' placeholder='Nombre' />
+                )}
+              </userForm.AppField>
 
-            <userForm.AppField name='password'>
-              {(field) => <field.PasswordTextField label='Contraseña' />}
-            </userForm.AppField>
-          </div>
-          <DialogFooter className='flex gap-4 justify-between'>
-            <DialogClose asChild>
-              <Button type='button' variant='secondary'>
-                Cerrar
-              </Button>
-            </DialogClose>
-            <userForm.AppForm>
-              <userForm.SubscribeButton
-                label='Grabar'
-                className='w-fit font-bold'
-              />
-            </userForm.AppForm>
-          </DialogFooter>
-        </form>
+              <userForm.AppField name='password'>
+                {(field) => <field.PasswordTextField label='Contraseña' />}
+              </userForm.AppField>
+            </div>
+            <DialogFooter className='flex gap-4 justify-between'>
+              <DialogClose asChild>
+                <Button type='button' variant='secondary'>
+                  Cerrar
+                </Button>
+              </DialogClose>
+              <userForm.AppForm>
+                <userForm.SubscribeButton
+                  label='Grabar'
+                  className='w-fit font-bold'
+                />
+              </userForm.AppForm>
+            </DialogFooter>
+          </form>
+        </DialogDescription>
       </DialogContent>
     </Dialog>
   )
