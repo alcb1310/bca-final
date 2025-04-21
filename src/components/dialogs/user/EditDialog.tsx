775acb846e26e048d9c24916209b9bc2d6f5c722
Andres Court <andres@andrescourt.com>
2025-04-21 13:29:28 -0500

    feat(user): display the edit user form

--- src/components/dialogs/user/EditDialog.tsx
+++ src/components/dialogs/user/EditDialog.tsx
@@ -3,37 +3,76 @@ import {
   Dialog,
   DialogClose,
   DialogContent,
+  DialogDescription,
   DialogFooter,
   DialogTitle,
   DialogTrigger,
 } from '@/components/ui/dialog'
+import { useAppForm } from '@/hooks/bca.form'
 import type { UserResponseType } from '@/types/users'
 import { Pencil } from 'lucide-react'
 
 export default function EditDialog({
   user,
 }: Readonly<{ user: UserResponseType }>) {
+  const userForm = useAppForm({
+    defaultValues: user,
+    onSubmit: ({ value }) => {
+      console.log(value)
+    },
+  })
+
   return (
     <Dialog>
       <DialogTrigger asChild>
-        <Pencil
-          size={12}
-          className='text-warning cursor-pointer'
-          onClick={() => {
-            console.log(user.id)
-          }}
-        />
+        <Button variant='ghost' size='icon'>
+          <Pencil
+            size={12}
+            className='text-warning cursor-pointer'
+            onClick={() => {
+              console.log(user.id)
+            }}
+          />
+        </Button>
       </DialogTrigger>
 
       <DialogContent>
         <DialogTitle>Editar usuario</DialogTitle>
-        <DialogFooter>
-          <DialogClose>
-            <Button type='button' variant='secondary'>
-              Cerrar
-            </Button>
-          </DialogClose>
-        </DialogFooter>
+        <DialogDescription asChild>
+          <form
+            onSubmit={(e) => {
+              e.preventDefault()
+              e.stopPropagation()
+
+              userForm.handleSubmit()
+            }}
+          >
+            <div className='mb-4 flex flex-col gap-4'>
+              <userForm.AppField name='email'>
+                {(field) => <field.TextField label='Email' />}
+              </userForm.AppField>
+
+              <userForm.AppField name='name'>
+                {(field) => <field.TextField label='Nombre' />}
+              </userForm.AppField>
+            </div>
+
+            <DialogFooter>
+              <DialogClose>
+                <Button type='button' variant='secondary'>
+                  Cerrar
+                </Button>
+              </DialogClose>
+
+              <userForm.AppForm>
+                <userForm.SubscribeButton
+                  label='Grabar'
+                  className='w-fit font-bold'
+                />
+              </userForm.AppForm>
+            </DialogFooter>
+          </form>
+        </DialogDescription>
       </DialogContent>
     </Dialog>
   )
