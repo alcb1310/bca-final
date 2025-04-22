10990f38f0d581a90f12c8b648a46146aff7cd4d
Andres Court <andres@andrescourt.com>
2025-04-22 17:38:10 -0500

    feat(query): get all projects from the backend

---
+++ src/queries/settings/projects.ts
@@ -0,0 +1,22 @@
+import type { ProjectResponseType } from '@/types/settings/projects'
+
+const url = import.meta.env.VITE_SERVER_URL
+
+if (!url) throw new Error('VITE_SERVER_URL is not defined')
+
+export async function getAllProjects({ token }: Readonly<{ token: string }>) {
+  const response = await fetch(`${url}/parametros/proyectos`, {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+      Authorization: `Bearer ${token}`,
+    },
+  })
+
+  if (!response.ok) {
+    const error = await response.json()
+    throw new Error(error.error)
+  }
+
+  return (await response.json()) as ProjectResponseType[]
+}
