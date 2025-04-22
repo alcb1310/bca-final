e3f33ca0f5a7a18f393250fdd2bdc214f26569a0
Andres Court <andres@andrescourt.com>
2025-04-22 17:38:36 -0500

    feat(projects): display all the projeccts

--- src/routes/_auth/parametros/proyectos.tsx
+++ src/routes/_auth/parametros/proyectos.tsx
@@ -1,9 +1,72 @@
-import { createFileRoute } from '@tanstack/react-router'
+import PageTitle from '@/components/titles/PageTitle'
+import { DataTable } from '@/components/ui/DataTable'
+import { getAllProjects } from '@/queries/settings/projects'
+import type { ProjectResponseType } from '@/types/settings/projects'
+import { useSuspenseQuery } from '@tanstack/react-query'
+import { createFileRoute, redirect } from '@tanstack/react-router'
+import type { ColumnDef } from '@tanstack/react-table'
 
 export const Route = createFileRoute('/_auth/parametros/proyectos')({
   component: RouteComponent,
+  loader: async ({ context: { queryClient, auth } }) => {
+    const token = auth.token
+    if (!token) throw redirect({ to: '/login' })
+
+    await queryClient.prefetchQuery({
+      queryKey: ['projects'],
+      queryFn: () => getAllProjects({ token }),
+    })
+
+    return { token, queryClient }
+  },
 })
 
 function RouteComponent() {
-  return <div>Hello "/_auth/parametros/proyectos"!</div>
+  const { token } = Route.useLoaderData()
+  const { data } = useSuspenseQuery({
+    queryKey: ['projects'],
+    queryFn: () => getAllProjects({ token: token }),
+  })
+
+  const columns: ColumnDef<ProjectResponseType>[] = [
+    {
+      header: () => <p className='font-bold text-center'>Nombre</p>,
+      accessorKey: 'name',
+    },
+    {
+      header: () => <div className='font-bold text-right'>Area Neta</div>,
+      accessorKey: 'net_area',
+      cell: ({ row }) => {
+        return (
+          <span className='block text-right '>
+            {row.original.gross_area.toLocaleString('es-EC', {
+              minimumFractionDigits: 2,
+              maximumFractionDigits: 2,
+            })}
+          </span>
+        )
+      },
+    },
+    {
+      header: () => <div className='font-bold text-right'>Area Bruta</div>,
+      accessorKey: 'gross_area',
+      cell: ({ row }) => {
+        return (
+          <span className='block text-right '>
+            {row.original.gross_area.toLocaleString('es-EC', {
+              minimumFractionDigits: 2,
+              maximumFractionDigits: 2,
+            })}
+          </span>
+        )
+      },
+    },
+  ]
+
+  return (
+    <div>
+      <PageTitle title='Proyectos' />
+      <DataTable data={data} columns={columns} />
+    </div>
+  )
 }
