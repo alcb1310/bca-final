40cd5720746b86c2a484cafe6b8c1b7c640d097a
Andres Court <andres@andrescourt.com>
2025-04-28 16:15:33 -0500

    feat(validation): added number validation

--- src/types/settings/projects.ts
+++ src/types/settings/projects.ts
@@ -4,8 +4,12 @@ export const ProjectResponseSchema = z.object({
   id: z.string().uuid(),
   name: z.string(),
   is_active: z.boolean(),
-  gross_area: z.number(),
-  net_area: z.number(),
+  gross_area: z.coerce.number({ message: 'Ingrese un numero valido' }).min(0, {
+    message: 'Ingrese un numero valido',
+  }),
+  net_area: z.coerce.number({ message: 'Ingrese un numero valido' }).min(0, {
+    message: 'Ingrese un numero valido',
+  }),
 })
 
 export type ProjectResponseType = z.infer<typeof ProjectResponseSchema>
