8d92d7e5cf129c6d45c08ee1f1caa51705513cd7
Andres Court <andres@andrescourt.com>
2025-04-28 19:02:56 -0500

    feat: porjet create schema

--- src/types/settings/projects.ts
+++ src/types/settings/projects.ts
@@ -13,3 +13,16 @@ export const ProjectResponseSchema = z.object({
 })
 
 export type ProjectResponseType = z.infer<typeof ProjectResponseSchema>
+
+export const ProjectCreateSchema = z.object({
+  name: z.string().min(1, { message: 'Ingrese un nombre valido' }),
+  is_active: z.boolean(),
+  gross_area: z.coerce.number({ message: 'Ingrese un numero valido' }).min(0, {
+    message: 'Ingrese un numero valido',
+  }),
+  net_area: z.coerce.number({ message: 'Ingrese un numero valido' }).min(0, {
+    message: 'Ingrese un numero valido',
+  }),
+})
+
+export type ProjectCreateType = z.infer<typeof ProjectCreateSchema>
