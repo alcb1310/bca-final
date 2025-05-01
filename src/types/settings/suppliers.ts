fb07d8e8294eefe6012ea3c8a92a07ec92f017d3
Andres Court <andres@andrescourt.com>
2025-04-30 22:35:09 -0500

    feat(proveedores): added grouping header and initial style

--- src/types/settings/suppliers.ts
+++ src/types/settings/suppliers.ts
@@ -4,9 +4,18 @@ export const SupplierResponseSchema = z.object({
   id: z.string().uuid(),
   supplier_id: z.string(),
   name: z.string(),
-  contact_name: z.string().optional(),
-  contact_phone: z.string().optional(),
-  contact_email: z.string().optional(),
+  contact_name: z.object({
+    String: z.string(),
+    Valid: z.boolean(),
+  }),
+  contact_phone: z.object({
+    String: z.string(),
+    Valid: z.boolean(),
+  }),
+  contact_email: z.object({
+    String: z.string(),
+    Valid: z.boolean(),
+  }),
 })
 
 export type SupplierResponseType = z.infer<typeof SupplierResponseSchema>
