6cb6677b4fca97170f7ca95abe5fad32798f085f
Andres Court <andres@andrescourt.com>
2025-05-03 11:58:46 -0500

    feat(categories): get all categories query

---
+++ src/queries/settings/categories.ts
@@ -0,0 +1,21 @@
+import type { CategoriesResponseType } from '@/types/settings/categories'
+
+const url = import.meta.env.VITE_SERVER_URL
+if (!url) throw new Error('VITE_SERVER_URL is not defined')
+
+export async function getAllCategories({ token }: Readonly<{ token: string }>) {
+  const response = await fetch(`${url}/parametros/categorias`, {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+      Authorization: `Bearer ${token}`,
+    },
+  })
+
+  if (!response.ok) {
+    const error = await response.json()
+    throw new Error(error.error)
+  }
+
+  return (await response.json()) as CategoriesResponseType[]
+}
