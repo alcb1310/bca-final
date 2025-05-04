bfdde972ef5cd744023a156a3c033be1d787302b
Andres Court <andres@andrescourt.com>
2025-05-04 10:30:12 -0500

    feat(materials): display all materials

--- src/routes/_auth/parametros/materiales.tsx
+++ src/routes/_auth/parametros/materiales.tsx
@@ -1,9 +1,75 @@
-import { createFileRoute } from '@tanstack/react-router'
+import PageTitle from '@/components/titles/PageTitle'
+import { DataTable } from '@/components/ui/DataTable'
+import getAllMaterials from '@/queries/settings/materials'
+import type { MaterialsResponseType } from '@/types/settings/materials'
+import { useSuspenseQuery } from '@tanstack/react-query'
+import { createFileRoute, redirect } from '@tanstack/react-router'
+import type { ColumnDef } from '@tanstack/react-table'
+import { Pencil } from 'lucide-react'
 
 export const Route = createFileRoute('/_auth/parametros/materiales')({
   component: RouteComponent,
+  loader: async ({ context: { queryClient, auth } }) => {
+    const token = auth.token
+    if (!token) throw redirect({ to: '/login' })
+
+    await queryClient.prefetchQuery({
+      queryKey: ['materials'],
+      queryFn: () => getAllMaterials({ token }),
+    })
+
+    return { token }
+  },
 })
 
 function RouteComponent() {
-  return <div>Hello "/_auth/parametros/materiales"!</div>
+  const { token } = Route.useLoaderData()
+  const { data } = useSuspenseQuery({
+    queryKey: ['materials'],
+    queryFn: () => getAllMaterials({ token }),
+  })
+
+  const columns: ColumnDef<MaterialsResponseType>[] = [
+    {
+      header: 'Codigo',
+      accessorKey: 'code',
+      minSize: 50,
+      maxSize: 100,
+      size: 50,
+    },
+    {
+      header: 'Nombre',
+      accessorKey: 'name',
+    },
+    {
+      header: 'Unidad',
+      accessorKey: 'unit',
+      minSize: 25,
+      maxSize: 50,
+      size: 25,
+    },
+    {
+      header: 'Categoria',
+      accessorKey: 'category.name',
+    },
+    {
+      id: 'actions',
+      size: 50,
+      enableSorting: false,
+      enableHiding: false,
+      cell: ({ row: { original } }) => {
+        console.log(original)
+        // TODO: Add edit material
+        return <Pencil className='text-warning' />
+      },
+    },
+  ]
+
+  // TODO: Add create material
+  return (
+    <div>
+      <PageTitle title='Materiales' />
+      <DataTable data={data} columns={columns} />
+    </div>
+  )
 }
