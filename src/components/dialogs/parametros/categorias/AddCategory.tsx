b071b235f3974b34ddc15f9a0a8544b58e2ae459
Andres Court <andres@andrescourt.com>
2025-05-03 21:39:06 -0500

    feat(category): save the new category to the backend

--- src/components/dialogs/parametros/categorias/AddCategory.tsx
+++ src/components/dialogs/parametros/categorias/AddCategory.tsx
@@ -8,20 +8,47 @@ import {
   DialogTrigger,
 } from '@/components/ui/dialog'
 import { useAppForm } from '@/hooks/bca.form'
+import { createCategory } from '@/queries/settings/categories'
+import { useAuth } from '@/utils/auth'
 import { DialogClose } from '@radix-ui/react-dialog'
+import { useMutation, useQueryClient } from '@tanstack/react-query'
 import { Plus } from 'lucide-react'
+import { useState } from 'react'
+import { toast } from 'sonner'
 
 export default function AddCategory() {
+  const [open, setOpen] = useState(false)
+  const { token } = useAuth()
+  const queryClient = useQueryClient()
+  const { mutate } = useMutation({
+    mutationFn: createCategory,
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ['categories'] })
+      toast('Categoria creada')
+      setOpen(false)
+    },
+    onError: (error) => {
+      toast.error(`Error al crear la categoria: ${error.message}`)
+    },
+  })
   const categoryForm = useAppForm({
     defaultValues: {
       name: '',
     },
+    onSubmit: ({ value }) => {
+      if (!token) throw new Error('No token')
+      mutate({ token, category: value })
+    },
   })
 
   return (
-    <Dialog>
+    <Dialog open={open} onOpenChange={setOpen}>
       <DialogTrigger asChild>
-        <Button variant='ghost' className='flex gap-2 items-center mb-4'>
+        <Button
+          variant='ghost'
+          className='flex gap-2 items-center mb-4'
+          onClick={() => categoryForm.reset()}
+        >
           <Plus />
           Crear categoria
         </Button>
