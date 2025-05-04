f79dfd3cef4f76d6140242f213d5460182dbe9ae
Andres Court <andres@andrescourt.com>
2025-05-04 10:19:14 -0500

    feat(query): get all materials query

---
+++ src/queries/settings/materials.ts
@@ -0,0 +1,23 @@
+import type { MaterialsResponseType } from '@/types/settings/materials'
+
+const url = import.meta.env.VITE_SERVER_URL
+if (!url) throw new Error('VITE_SERVER_URL is not defined')
+
+export default async function getAllMaterials({
+  token,
+}: Readonly<{ token: string }>) {
+  const response = await fetch(`${url}/parametros/materiales`, {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+      Authorization: `Bearer ${token}`,
+    },
+  })
+
+  if (!response.ok) {
+    const error = await response.json()
+    throw new Error(error.error)
+  }
+
+  return (await response.json()) as MaterialsResponseType[]
+}
