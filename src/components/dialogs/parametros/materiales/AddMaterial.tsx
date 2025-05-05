2a2c897a4528d043c1a6bf517b710d8ddde964ae
Andres Court <andres@andrescourt.com>
2025-05-04 21:23:10 -0500

    feat(materials): display the create material form

--- src/components/dialogs/parametros/materiales/AddMaterial.tsx
+++ src/components/dialogs/parametros/materiales/AddMaterial.tsx
@@ -8,13 +8,51 @@ import {
   DialogTitle,
   DialogTrigger,
 } from '@/components/ui/dialog'
+import { useAppForm } from '@/hooks/bca.form'
+import { getAllCategories } from '@/queries/settings/categories'
+import { MaterialsCreateSchema } from '@/types/settings/materials'
+import { useAuth } from '@/utils/auth'
+import { useQuery } from '@tanstack/react-query'
 import { Plus } from 'lucide-react'
+import { useState } from 'react'
 
 export default function AddMaterial() {
+  const [open, setOpen] = useState(false)
+  const { token } = useAuth()
+  const materialForm = useAppForm({
+    defaultValues: {
+      code: '',
+      name: '',
+      unit: '',
+      category: '',
+    },
+    validators: {
+      onSubmit: MaterialsCreateSchema,
+    },
+  })
+  const { data: categoriesQuery } = useQuery({
+    queryKey: ['categories'],
+    queryFn: () => {
+      if (!token) throw new Error('No token')
+      return getAllCategories({ token })
+    },
+  })
+
+  const categories = categoriesQuery
+    ? categoriesQuery.map((material) => ({
+      value: material.id,
+      label: material.name,
+    }))
+    : []
+
   return (
-    <Dialog>
+    <Dialog open={open} onOpenChange={setOpen}>
       <DialogTrigger asChild>
-        <Button variant={'ghost'} className='flex gap-2 items-center mb-4'>
+        <Button
+          variant={'ghost'}
+          className='flex gap-2 items-center mb-4'
+          onClick={() => materialForm.reset()}
+        >
           <Plus />
           Agregar Material
         </Button>
@@ -22,11 +60,50 @@ export default function AddMaterial() {
       <DialogContent>
         <DialogTitle>Crear Material</DialogTitle>
         <DialogDescription>
-          <DialogFooter>
-            <DialogClose asChild>
-              <Button variant={'secondary'}>Cerrar</Button>
-            </DialogClose>
-          </DialogFooter>
+          <form
+            onSubmit={(e) => {
+              e.preventDefault()
+              e.stopPropagation()
+              materialForm.handleSubmit()
+            }}
+          >
+            <div className='flex flex-col gap-4 mb-4'>
+              <materialForm.AppField name='category'>
+                {(field) => (
+                  <field.SelectField label='Categoría' values={categories} />
+                )}
+              </materialForm.AppField>
+
+              <materialForm.AppField name='code'>
+                {(field) => (
+                  <field.TextField label='Código' placeholder='Código' />
+                )}
+              </materialForm.AppField>
+
+              <materialForm.AppField name='name'>
+                {(field) => (
+                  <field.TextField label='Nombre' placeholder='Nombre' />
+                )}
+              </materialForm.AppField>
+
+              <materialForm.AppField name='unit'>
+                {(field) => (
+                  <field.TextField label='Unidad' placeholder='Unidad' />
+                )}
+              </materialForm.AppField>
+            </div>
+            <DialogFooter>
+              <DialogClose asChild>
+                <Button variant={'secondary'}>Cerrar</Button>
+              </DialogClose>
+              <materialForm.AppForm>
+                <materialForm.SubscribeButton
+                  label='Guardar'
+                  className='w-fit'
+                />
+              </materialForm.AppForm>
+            </DialogFooter>
+          </form>
         </DialogDescription>
       </DialogContent>
     </Dialog>
