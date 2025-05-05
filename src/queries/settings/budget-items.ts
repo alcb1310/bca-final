8175227697b6c4cf70c24b44ee424b9e3a33c08b
Andres Court <andres@andrescourt.com>
2025-05-04 22:17:03 -0500

    feat(budget-item): added the get all budget-items query

---
+++ src/queries/settings/budget-items.ts
@@ -0,0 +1,23 @@
+import type { BudgetItemResponseType } from '@/types/settings/budget-items'
+
+const url = import.meta.env.VITE_SERVER_URL
+if (!url) throw new Error('VITE_SERVER_URL is not defined')
+
+export async function getAllBudgetItems({
+  token,
+}: Readonly<{ token: string }>) {
+  const response = await fetch(`${url}/parametros/partidas`, {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+      Authorization: `Bearer ${token}`,
+    },
+  })
+
+  if (!response.ok) {
+    const error = await response.json()
+    throw new Error(error.error)
+  }
+
+  return (await response.json()) as BudgetItemResponseType[]
+}
