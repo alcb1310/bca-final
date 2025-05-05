37ac4def521e6c15005b0e66afa062f29429e996
Andres Court <andres@andrescourt.com>
2025-05-05 15:49:17 -0500

    feat(budget-items): added a form in the dialog

--- src/components/dialogs/parametros/partidas/EditBudgetItem.tsx
+++ src/components/dialogs/parametros/partidas/EditBudgetItem.tsx
@@ -8,9 +8,45 @@ import {
   DialogTitle,
   DialogTrigger,
 } from '@/components/ui/dialog'
+import { useAppForm } from '@/hooks/bca.form'
+import { getAllBudgetItemByAccumulate } from '@/queries/settings/budget-items'
+import type { BudgetItemResponseType } from '@/types/settings/budget-items'
+import { useAuth } from '@/utils/auth'
+import { useQuery } from '@tanstack/react-query'
 import { Pencil } from 'lucide-react'
 
-export default function EditBudgetItem() {
+export default function EditBudgetItem({
+  budgetItem,
+}: Readonly<{ budgetItem: BudgetItemResponseType }>) {
+  const { token } = useAuth()
+  const budgetItemForm = useAppForm({
+    defaultValues: {
+      id: budgetItem.id,
+      code: budgetItem.code,
+      name: budgetItem.name,
+      accumulate: budgetItem.accumulate,
+      parent_id: budgetItem.parent?.id,
+    },
+  })
+  const { data: parentBudgetItems } = useQuery({
+    queryKey: ['budget-items', 'parent'],
+    queryFn: () => {
+      if (!token) throw new Error('No token')
+      return getAllBudgetItemByAccumulate({ token, accumulate: true })
+    },
+  })
+
+  const parentItems = parentBudgetItems
+    ? parentBudgetItems.map((item) => {
+      return {
+        value: item.id,
+        label: item.name,
+      }
+    })
+    : []
+
+  parentItems.unshift({ value: '', label: 'Ninguno' })
+
   return (
     <Dialog>
       <DialogTrigger asChild>
@@ -22,11 +58,19 @@ export default function EditBudgetItem() {
       <DialogContent>
         <DialogTitle>Editar Partida</DialogTitle>
         <DialogDescription asChild>
-          <DialogFooter>
-            <DialogClose asChild>
-              <Button variant={'secondary'}>Cancelar</Button>
-            </DialogClose>
-          </DialogFooter>
+          <form
+            onSubmit={(e) => {
+              e.preventDefault()
+              e.stopPropagation()
+              budgetItemForm.handleSubmit()
+            }}
+          >
+            <DialogFooter>
+              <DialogClose asChild>
+                <Button variant={'secondary'}>Cancelar</Button>
+              </DialogClose>
+            </DialogFooter>
+          </form>
         </DialogDescription>
       </DialogContent>
     </Dialog>
