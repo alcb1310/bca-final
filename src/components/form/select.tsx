1a3eb5f3e37d23a7cc93237c5b2165004bb16523
Andres Court <andres@andrescourt.com>
2025-05-04 20:12:39 -0500

    feat(select): add label and disabled

--- src/components/form/select.tsx
+++ src/components/form/select.tsx
@@ -1,3 +1,4 @@
+import { Label } from '@/components/ui/label'
 import * as ShadcnSelect from '@/components/ui/select'
 import { useFieldContext } from '@/hooks/bca.form'
 import { useStore } from '@tanstack/react-form'
@@ -7,20 +8,26 @@ export function SelectField({
   label,
   values,
   placeholder,
+  disabled = false,
 }: Readonly<{
   label: string
   values: Array<{ value: string; label: string }>
   placeholder?: string
+  disabled?: boolean
 }>) {
   const field = useFieldContext<string>()
   const errors = useStore(field.store, (state) => state.meta.errors)
 
   return (
     <div>
+      <Label htmlFor={label} className='mb-1 font-bold'>
+        {label}
+      </Label>
       <ShadcnSelect.Select
         name={field.name}
         value={field.state.value}
         onValueChange={(value) => field.handleChange(value)}
+        disabled={disabled}
       >
         <ShadcnSelect.SelectTrigger className='w-full'>
           <ShadcnSelect.SelectValue placeholder={placeholder} />
@@ -28,7 +35,6 @@ export function SelectField({
 
         <ShadcnSelect.SelectContent>
           <ShadcnSelect.SelectGroup>
-            <ShadcnSelect.SelectLabel>{label}</ShadcnSelect.SelectLabel>
             {values.map(({ value, label }) => (
               <ShadcnSelect.SelectItem key={value} value={value}>
                 {label}
