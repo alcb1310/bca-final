a53d5ac68a35bfba4152bb3864c51eda6bf2710f
Andres Court <andres@andrescourt.com>
2025-05-09 17:43:25 -0500

    feat(AddBudgetItem): added the form to create the budget item

--- src/components/dialogs/parametros/partidas/AddBudgetItem.tsx
+++ src/components/dialogs/parametros/partidas/AddBudgetItem.tsx
@@ -8,9 +8,44 @@ import {
   DialogTitle,
   DialogTrigger,
 } from '@/components/ui/dialog'
+import { useAppForm } from '@/hooks/bca.form'
+import { getAllBudgetItemByAccumulate } from '@/queries/settings/budget-items'
+import { useAuth } from '@/utils/auth'
+import { useSuspenseQuery } from '@tanstack/react-query'
 import { Plus } from 'lucide-react'
 
 export default function AddBudgetItem() {
+  const { token } = useAuth()
+  const { data: parentBudgetItems } = useSuspenseQuery({
+    queryKey: ['categories', 'parent'],
+    queryFn: () => {
+      if (!token) throw new Error('No token')
+      return getAllBudgetItemByAccumulate({ token, accum: true })
+    },
+  })
+
+  const budgetItemForm = useAppForm({
+    defaultValues: {
+      code: '',
+      name: '',
+      accumulate: false,
+      parent_id: undefined,
+    },
+    onSubmit: async ({ value }) => {
+      if (!token) throw new Error('No token')
+      console.log('submittig', value)
+    },
+  })
+
+  const parentItems = parentBudgetItems
+    ? parentBudgetItems.map((item) => {
+        return {
+          value: item.id,
+          label: item.name,
+        }
+      })
+    : []
+
   return (
     <Dialog>
       <DialogTrigger asChild>
@@ -23,11 +58,45 @@ export default function AddBudgetItem() {
       <DialogContent>
         <DialogTitle>Crear partida</DialogTitle>
         <DialogDescription asChild>
-          <DialogFooter>
-            <DialogClose asChild>
-              <Button variant={'secondary'}>Cerrar</Button>
-            </DialogClose>
-          </DialogFooter>
+          <form
+            onSubmit={(e) => {
+              e.preventDefault()
+              e.stopPropagation()
+              budgetItemForm.handleSubmit()
+            }}
+          >
+            <div className='flex flex-col gap-4 mb-4'>
+              <budgetItemForm.AppField name='parent_id'>
+                {(field) => (
+                  <field.SelectField values={parentItems} label='Padre' />
+                )}
+              </budgetItemForm.AppField>
+
+              <budgetItemForm.AppField name='code'>
+                {(field) => <field.TextField label='Codigo' />}
+              </budgetItemForm.AppField>
+
+              <budgetItemForm.AppField name='name'>
+                {(field) => <field.TextField label='Nombre' />}
+              </budgetItemForm.AppField>
+
+              <budgetItemForm.AppField name='accumulate'>
+                {(field) => <field.SwitchField label='Accumula' />}
+              </budgetItemForm.AppField>
+            </div>
+
+            <DialogFooter>
+              <DialogClose asChild>
+                <Button variant={'secondary'}>Cerrar</Button>
+              </DialogClose>
+              <budgetItemForm.AppForm>
+                <budgetItemForm.SubscribeButton
+                  label='Guardar'
+                  className='w-fit'
+                />
+              </budgetItemForm.AppForm>
+            </DialogFooter>
+          </form>
         </DialogDescription>
       </DialogContent>
     </Dialog>
